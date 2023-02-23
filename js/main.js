853f0195d37d64e29e591393273c5193081f51f7
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-02-24 00:12:52 +0100

    manca costo biglietto e abbellimenti

--- js/main.js
+++ js/main.js
@@ -1,36 +1,52 @@
-     const nomeCognome = document.getElementById('nomeCognome');
-          
-     const numeroChilometri = document.getElementById('kmDaPercorrere');
-
-     const tipoBiglietto = document.getElementById('fasciaEta') 
+    const nomeCognome = document.getElementById('nomeCognome'); // COSTANTE NOME E COGNOME 
+    const selezioneValore = document.getElementById('casellaSelezione'); //COSTANTE VALORE SELEZIONE
+    const chilometri = document.getElementById('kmDaPercorrere'); //COSTANTE SCELTA CHILOMETRI
+    const costoPerchilometro = 0.21; //COSTANET COSTO PER CHILOMETRO
+    const prezzoBase = chilometri * costoPerchilometro;// COSTANTE COSTO BIGLIETTO SENZA SCONTI 
+    const calcoloSconto20 = (prezzoBase * 20 / 100);// SCONTO APPLICATO 20%
+    const calcoloSconto40 = (prezzoBase * 40 / 100);// SCONTO APPLICATO 40%
+    const totaleconSconto20 = (prezzoBase - calcoloSconto20);// PREZZO TOTALE CON SCONTO DEL 20% PER MINORI DI 18 ANNI
+    const totaleconSconto40 = (prezzoBase - calcoloSconto40);// PREZZO TOTALE CON SCONTO DEL 40% PER MAGGIORI DI 65 ANNI
+    const numeroCarrozza = Math.floor((Math.random() * 10)) + 1;
+    const codiceCp = Math.floor((Math.random() * 90000)) + 10000;
      
-     const prezzoKm = 0.21; 
-     
-     const prezzoViaggio = numeroChilometri * prezzoKm;
+   
+    
 
-     const  minorenne = (prezzoViaggio * 20 / 100) 
-     
-     const  over = (prezzoViaggio * 40 / 100)     
-  
-     const totalMino = (prezzoViaggio - minorenne); 
+//BOTTONE PER INVIO DATI AL JS
+    const inviaRichiesta = document.getElementById('inviaForm');
+          inviaRichiesta.addEventListener('click',
+          function() {
+            console.log(nomeCognome.value);
+            console.log(selezioneValore.value);
+            console.log(chilometri.value);
+            console.log(numeroCarrozza);
+            console.log(codiceCp);
+            document.getElementById('risultato').innerHTML = (nomeCognome.value); 
+            document.getElementById('numeroCarrozza').innerHTML = (numeroCarrozza);
+            document.getElementById('codiceCp').innerHTML = (codiceCp); 
+            if (selezioneValore.value == 'bigliettoUnder18'){
+                document.getElementById('typeTicket').innerHTML = "Biglietto ridotto del 20%";
+            } else if (selezioneValore.value == 'bigliettoOver65'){
+                document.getElementById('typeTicket').innerHTML = "Biglietto ridotto del 40%";
+            } else{
+                document.getElementById('typeTicket').innerHTML = "Biglietto Standard";
+            };
 
-     const totalOver = (prezzoViaggio - over); 
-
-     const bigliettoMinorenne =document.getElementById('minorenne'); 
-
+            if (selezioneValore.value == 'bigliettoUnder18')
+          }
 
 
-
-const generaDom = document.getElementById('buttonGenera');
-      generaDom.addEventListener('click', 
-      function () { 
-         console.log(nomeCognome.value)
-         console.log(numeroChilometri.value);
-         console.log(tipoBiglietto.value)
-      }
-      );
-
-
-if (tipoBiglietto.value = minorenne){
-    document.getElementById('prezzo').innerHTML = (totalMino.toFixed(2));
-}    
+          );
+//BOTTONE PER AZZERAMENTO CAMPI          
+          const reset = document.getElementById('resetForm');      
+          reset.addEventListener('click',
+          function() {
+            const azzeramentoCampi = document.getElementById('nomeCognome');
+            const azzeramentoCampi1 = document.getElementById('casellaSelezione');
+            const azzeramentoCampi2 = document.getElementById('kmDaPercorrere'); 
+            azzeramentoCampi.value = "";
+            azzeramentoCampi1.value = "";
+            azzeramentoCampi2.value = "";
+          }
+          );
